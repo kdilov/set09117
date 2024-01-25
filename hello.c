161f538ac64d6540e97a4e42be90f0304405380f
Konstantin <k.dilyanov@gmail.com>
2024-01-25 13:23:14 +0000

    Updated Hello.c file

--- hello.c
+++ hello.c
@@ -12,5 +12,14 @@ int main(int argc,char **argv)
     printf("How old are you?");
     scanf("%d", &age);
 
-    printf("You are  %d years old and your name is %s", age,name);
+    if (age >= 18){
+        printf("You are singed up!");
+    }
+    else if(age < 0){
+        printf("Enter a valid value");
+    }
+    else{
+        printf("You are too young to sign up!");
+    }
+    return 0;
 }
