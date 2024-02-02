917669010fb21ddf2fc1585732138435eba88f03
Konstantin <k.dilyanov@gmail.com>
2024-02-02 16:24:12 +0000

    Timings working as intended

--- set09117/timings1.c
+++ set09117/timings1.c
@@ -19,7 +19,7 @@ int main()
     double total_time = 0.0;
     
     // Open file for writing
-    fp = fopen("output.csv", "w");
+    fp = fopen("output.csv", "a");
     if(fp ==NULL){
         printf("can not open file\n");
         return 1;
@@ -39,12 +39,12 @@ int main()
     printf("Elapsed: %lf seconds\n", elapsed=((double)t) / CLOCKS_PER_SEC );
 
     printf("Average: %.10lf seconds \n",average= elapsed/num_runs);
-    printf(" Elapsed time:\t  Average time:\t \n");
-    printf("   %.10lf\t\t %.10lf\n",elapsed,average);
+    printf("Times Code is Run: \t Elapsed time:\t  Average time:\t \n");
+    printf("%d\t\t\t   %.10lf\t %.10lf\n",num_runs,elapsed,average);
     
     // Write the formatted data to the file
-    fprintf(fp," Elapsed time:\t  Average time:\t \n");
-    fprintf(fp,"   %.10lf,\t\t %.10lf\n",elapsed,average);
+    fprintf(fp,"Times Code is Run:\t Elapsed time:\t  Average time:\t \n");
+    fprintf(fp,"%d\t\t\t\t\t   %.10lf,\t\t %.10lf\n",num_runs,elapsed,average);
 
     fclose(fp);
     return 0;
